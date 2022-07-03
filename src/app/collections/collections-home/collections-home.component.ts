df1ac8c50d3d2ebe2a9db4ff08c4198209d9050c
Nattie <nkosin361@gmail.com>
2022-07-03 21:12:07 +0200

    Passing down table's data to child component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'Nattie', age: 26, job: 'Designer', employed: false },
+    { name: 'Philane', age: 25, job: 'Software Developer', employed: true },
+    { name: 'Charles', age: 27, job: 'Software Tester', employed: true }
+  ];
+
+  headers = [
+    { key: 'employed', label: 'Has a Job' },
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
 
   constructor() { }
 
