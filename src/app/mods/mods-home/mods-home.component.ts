4a6cd0d7119cd0ffeb3d916fedca300584402ac9
Nutty4616 <nkosin361@gmail.com>
2022-07-05 13:03:51 +0200

    Adding a reusable accordion component

--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModsHomeComponent implements OnInit {
   modalOpen = false;
+  items = [
+    { title: 'Why is the sky blue?', content: 'The sky is blue because it is' },
+    { title: 'What does the orange taste like?', content: 'The orange tastes like an orange' },
+    { title: 'What color is your dog?', content: 'The color of my dog is black' },
+  ];
 
   constructor() { }
 
